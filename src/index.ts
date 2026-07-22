9179f9c6e45575c9abc69e55a2eef7821c1e85d4
Mikhail Nepomniashchii <michey85@gmail.com>
2026-07-22 20:37:36 +0200

    project inti

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import server from "./server.js";
+
+declare const process: {
+  exit(code?: number): never;
+};
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Weather MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+import { z } from "zod";
+
+ const server = new McpServer({
+  name: "weather",
+  version: "1.0.0",
+});
+
+
+
+
+
+export default server;
